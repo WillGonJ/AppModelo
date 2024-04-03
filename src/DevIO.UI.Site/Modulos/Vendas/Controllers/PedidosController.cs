4fbeb3ed031ed16d6b68f9c46c7ab10431060b95
Willian Gonçalves de Jesus <will_kaiker@hotmail.com>
2024-04-03 12:43:51 -0300

    Finalizando Areas

---
+++ src/DevIO.UI.Site/Modulos/Vendas/Controllers/PedidosController.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.UI.Site.Modulos.Vendas.Controllers
+{
+    public class PedidosController : Controller
+    {
+        [Area("Vendas")]
+        public  IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
