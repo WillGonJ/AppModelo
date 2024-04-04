205f3a550ddc38cadbcd28d04821f0b2c98bf8f0
Willian Gonçalves de Jesus <will_kaiker@hotmail.com>
2024-04-04 12:28:47 -0300

    DbContext

---
+++ src/DevIO.UI.Site/Modulos/Vendas/Data/MeuDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.UI.Site.Modulos.Vendas.Data
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+
+}
